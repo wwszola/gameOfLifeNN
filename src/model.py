0255092964d882e0ab96369612158ee6d48a1c0f
wwszola <wwszola0@gmail.com>
2024-07-11 19:17:37 +0200

    add randomness seed to weights initializers

--- src/model.py
+++ src/model.py
@@ -1,13 +1,32 @@
 import tensorflow as tf
 import keras
 
-def build_model(board_shape, overcompleteness = 1):
+def build_model(board_shape, overcompleteness = 1, seed = None):
+    seed_gen = keras.random.SeedGenerator(seed)
     model = keras.Sequential()
     model.add(keras.layers.Input((*board_shape, 1)))
-    for _ in range(overcompleteness):
-        model.add(keras.layers.Conv2D(2, (3, 3), padding = "same", activation = "relu"))
-        model.add(keras.layers.Conv2D(1, (1, 1), padding = "same", activation = "relu"))
-    model.add(keras.layers.Conv2D(1, (1, 1), padding = "same", activation = "sigmoid"))
+    for i in range(overcompleteness):
+        model.add(keras.layers.Conv2D(
+            filters = 2, 
+            kernel_size = (3, 3), 
+            padding = "same", 
+            activation = "relu", 
+            kernel_initializer = keras.initializers.RandomUniform(-0.1, 0.1, seed_gen)
+        ))
+        model.add(keras.layers.Conv2D(
+            filters = 1, 
+            kernel_size = (1, 1), 
+            padding = "same", 
+            activation = "relu", 
+            kernel_initializer = keras.initializers.RandomUniform(-0.1, 0.1, seed_gen)
+        ))
+    model.add(keras.layers.Conv2D(
+        filters = 1, 
+        kernel_size = (1, 1), 
+        padding = "same", 
+        activation = "sigmoid",
+        kernel_initializer = keras.initializers.RandomUniform(-0.1, 0.1, seed_gen)
+    ))
 
     model.compile(
         optimizer = keras.optimizers.Adam(0.01, 0.9, 0.999),
--- src/train.py
+++ src/train.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     ds = random_uniform(0.38, (32766, *board_shape), 0)
     ds = ds.batch(16)
     for i in range(64):
-        model = build_model(board_shape, 3)
+        model = build_model(board_shape, 1, i)
         # model.summary()
 
         early_stopping = keras.callbacks.EarlyStopping(monitor="loss", patience=1, verbose=1)
@@ -19,7 +19,7 @@ if __name__ == "__main__":
             ds,
             epochs = 32,
             callbacks = [early_stopping],
-            verbose = 0
+            verbose = 1
         )
         loss = min(history.history["loss"])
         print(i, "done with loss", loss)
