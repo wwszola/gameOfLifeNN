46a41850f93493934f6d359a036e9ecd52159373
wwszola <wwszola0@gmail.com>
2024-07-13 22:04:52 +0200

    model completeness properly implemented

--- src/model.py
+++ src/model.py
@@ -5,27 +5,26 @@ def build_model(board_shape, overcompleteness = 1, seed = None, optimizer = "Ada
     seed_gen = keras.random.SeedGenerator(seed)
     model = keras.Sequential()
     model.add(keras.layers.Input((*board_shape, 1)))
-    for i in range(overcompleteness):
-        model.add(keras.layers.Conv2D(
-            filters = 2, 
-            kernel_size = (3, 3), 
-            padding = "same", 
-            activation = "relu", 
-            kernel_initializer = keras.initializers.RandomUniform(-0.1, 0.1, seed_gen)
-        ))
-        model.add(keras.layers.Conv2D(
-            filters = 1, 
-            kernel_size = (1, 1), 
-            padding = "same", 
-            activation = "relu", 
-            kernel_initializer = keras.initializers.RandomUniform(-0.1, 0.1, seed_gen)
-        ))
+    model.add(keras.layers.Conv2D(
+        filters = 2 * overcompleteness, 
+        kernel_size = (3, 3), 
+        padding = "same", 
+        activation = "relu", 
+        kernel_initializer = keras.initializers.RandomNormal(0., 1., seed_gen)
+    ))
+    model.add(keras.layers.Conv2D(
+        filters = overcompleteness, 
+        kernel_size = (1, 1), 
+        padding = "same", 
+        activation = "relu", 
+        kernel_initializer = keras.initializers.RandomUniform(0., 1., seed_gen)
+    ))
     model.add(keras.layers.Conv2D(
         filters = 1, 
         kernel_size = (1, 1), 
         padding = "same", 
         activation = "sigmoid",
-        kernel_initializer = keras.initializers.RandomUniform(-0.1, 0.1, seed_gen)
+        kernel_initializer = keras.initializers.RandomUniform(0., 1., seed_gen)
     ))
 
     model.compile(
