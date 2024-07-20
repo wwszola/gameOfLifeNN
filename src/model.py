11f09e9cb557447b9eac35e6b00311bd89eae957
wwszola <wwszola0@gmail.com>
2024-07-20 21:47:08 +0200

    works for saving and loading

--- src/model.py
+++ src/model.py
@@ -2,28 +2,27 @@ import tensorflow as tf
 import keras
 
 def build_model(overcompleteness = 1, seed = None, optimizer = "Adam"):
-    seed_gen = keras.random.SeedGenerator(seed)
     model = keras.Sequential()
     model.add(keras.layers.Conv2D(
         filters = 2 * overcompleteness, 
         kernel_size = (3, 3), 
         padding = "same", 
         activation = "relu", 
-        kernel_initializer = keras.initializers.RandomNormal(0., 1., seed_gen)
+        kernel_initializer = keras.initializers.RandomNormal(0., 1., hash(seed)+1)
     ))
     model.add(keras.layers.Conv2D(
         filters = overcompleteness, 
         kernel_size = (1, 1), 
         padding = "same", 
         activation = "relu", 
-        kernel_initializer = keras.initializers.RandomUniform(0., 1., seed_gen)
+        kernel_initializer = keras.initializers.RandomUniform(0., 1., hash(seed)+2)
     ))
     model.add(keras.layers.Conv2D(
         filters = 1, 
         kernel_size = (1, 1), 
         padding = "same", 
         activation = "sigmoid",
-        kernel_initializer = keras.initializers.RandomUniform(0., 1., seed_gen)
+        kernel_initializer = keras.initializers.RandomUniform(0., 1., hash(seed)+3)
     ))
 
     model.compile(
